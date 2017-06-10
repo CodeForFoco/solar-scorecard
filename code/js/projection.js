819da17ea53a1f25d7c9fc711786ed2a89f0495e
Dominick Gendill <dgendill@gmail.com>
2017-06-10 10:30:48 -0600

    Added .eslintrc.  Removed old code in goal.js and monthly.js.  Restyled projection.js.

--- code/js/projection.js
+++ code/js/projection.js
@@ -4,10 +4,9 @@
 d3.projectionChart = function projectionChart() {
 
   // Default Sizing
-  var width, height, data;
+  var width = 900, height = 500, data;
+
   var margin = {top: 30, right: 50, bottom: 60, left: 50};
-  setWidth(900);
-  setHeight(500);
 
   function setWidth(value) {
     width = value;
@@ -18,6 +17,9 @@ d3.projectionChart = function projectionChart() {
     height = value;
     innerHeight = height - margin.top - margin.bottom;
   }
+
+  setWidth(width);
+  setHeight(height);
 
   var updateView = function() {};
 
@@ -35,59 +37,65 @@ d3.projectionChart = function projectionChart() {
       .attr("width", width)
       .attr("height", height)
       .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        // Initial Rendering of chart...
-        var xData = d3.scaleTime()
-        .domain(d3.extent(data.all, function(d) { return d.date; }))
-        .range([0,innerWidth])
+      // Initial Rendering of chart...
+      var xData = d3.scaleTime()
+      .domain(d3.extent(data.all, function(d) {
+        return d.date;
+      }))
+      .range([0, innerWidth]);
 
-        var yData = d3.scaleLinear()
-        .domain(d3.extent(data.all, function(d) { return d.megawatts}))
-        .range([innerHeight, 0])
+      var yData = d3.scaleLinear()
+        .domain(d3.extent(data.all, function(d) {
+          return d.megawatts;
+        }))
+        .range([innerHeight, 0]);
 
-        var xAxis = d3.axisBottom(xData)
-        .ticks(10)
-        .tickFormat(d3.timeFormat("%Y"))
+      var xAxis = d3.axisBottom(xData)
+      .ticks(10)
+      .tickFormat(d3.timeFormat("%Y"));
 
-        var line = d3.line()
-          .x(function(d) { return xData(d.date);})
-          .y(function(d) { return yData(d.megawatts)})
+      var line = d3.line()
+        .x(function(d) { return xData(d.date);})
+        .y(function(d) {
+          return yData(d.megawatts);
+        });
 
-        var yAxis = d3.axisLeft(yData).ticks(10);
+      var yAxis = d3.axisLeft(yData).ticks(10);
 
-        chart.selectAll("g.xAxis")
-          .data(data.all)
-          .enter()
-          .append('g').attr('class', 'xAxis')
-          .attr("transform", "translate(0," + innerHeight + ")")
-          .call(xAxis)
+      chart.selectAll("g.xAxis")
+        .data(data.all)
+        .enter()
+        .append('g').attr('class', 'xAxis')
+        .attr("transform", "translate(0," + innerHeight + ")")
+        .call(xAxis);
 
-        chart.selectAll("g.yAxis")
-          .data(data.all)
-          .enter()
-          .append('g').attr('class', 'yAxis')
-          .call(yAxis)
+      chart.selectAll("g.yAxis")
+        .data(data.all)
+        .enter()
+        .append('g').attr('class', 'yAxis')
+        .call(yAxis);
 
-        chart.selectAll('path.past')
-          .data([data.past])
-          .enter()
-            .append('path')
-            .attr('class', 'past line')
-            .attr('stroke','black')
-            .attr('fill','none')
-            .attr('d', line)
-            .attr('stroke-width', 3)
-
-        chart.selectAll("path.future")
-          .data([data.currentFuture])
-          .enter()
+      chart.selectAll('path.past')
+        .data([data.past])
+        .enter()
           .append('path')
-          .attr('class', 'future line')
-          .attr('stroke', '#0f0')
+          .attr('class', 'past line')
+          .attr('stroke', 'black')
           .attr('fill', 'none')
           .attr('d', line)
-          .attr('stroke-width', 3)
+          .attr('stroke-width', 3);
+
+      chart.selectAll("path.future")
+        .data([data.currentFuture])
+        .enter()
+        .append('path')
+        .attr('class', 'future line')
+        .attr('stroke', '#0f0')
+        .attr('fill', 'none')
+        .attr('d', line)
+        .attr('stroke-width', 3);
 
       // Update the Chart
       updateView = function() {
@@ -95,73 +103,77 @@ d3.projectionChart = function projectionChart() {
         // Build Chart Scales and Components
         var xData = d3.scaleTime()
         .domain(d3.extent(data.all, function(d) { return d.date; }))
-        .range([0,innerWidth])
+        .range([0, innerWidth]);
 
         var yData = d3.scaleLinear()
-        .domain(d3.extent(data.all, function(d) { return d.megawatts}))
-        .range([innerHeight, 0])
+        .domain(d3.extent(data.all, function(d) {
+          return d.megawatts;
+        }))
+        .range([innerHeight, 0]);
 
         var xAxis = d3.axisBottom(xData)
         .ticks(10)
-        .tickFormat(d3.timeFormat("%Y"))
+        .tickFormat(d3.timeFormat("%Y"));
 
         var line = d3.line()
-          .x(function(d) { return xData(d.date);})
-          .y(function(d) { return yData(d.megawatts)})
+          .x(function(d) { return xData(d.date); })
+          .y(function(d) { return yData(d.megawatts); });
 
         var yAxis = d3.axisLeft(yData).ticks(10);
 
         chart.selectAll("g.xAxis")
           .transition()
           .duration(500)
-          .call(xAxis)
+          .call(xAxis);
 
         chart.selectAll("g.yAxis")
           .transition()
           .duration(500)
-          .call(yAxis)
+          .call(yAxis);
 
         d3.selectAll('path.future')
           .transition()
           .duration(500)
-          .attr('d', line)
+          .attr('d', line);
 
         d3.selectAll('path.past')
           .transition()
           .duration(500)
-          .attr('d', line)
+          .attr('d', line);
 
-      }
-    })
-  }
+      };
+    });
+  };
 
+  // Original Idea Back in 2012
   // http://bl.ocks.org/cpbotha/5073718
   // https://bost.ocks.org/mike/chart/
+
+  // Discussion on using same idea in version 4
   // https://groups.google.com/forum/#!topic/d3-js/Wh85AE_mS1Q
+  // Example using d3 version 4
   // https://bl.ocks.org/rcmoore38/9f2908455355c0589619
 
   i.data = function(d) {
     data = d;
     return i;
-  }
+  };
 
   i.width = function(value) {
     if (!arguments.length) return width;
     setWidth(value);
     return i;
-  }
+  };
 
   i.height = function(value) {
     if (!arguments.length) return height;
-    setHeight(value)
+    setHeight(value);
     return i;
-  }
+  };
 
   i.update = function() {
     updateView();
-  }
-
-
+  };
 
   return i;
 
@@ -189,10 +201,10 @@ d3.projectionChart.generateDummyData = function generateDummyData() {
   }
 
   var data = allYears.map(function(date, index) {
-      return {
-        date : date,
-        megawatts : d3.projectionChart.randomIntGen(index * 3, (index * 3)+15)()
-      }
+    return {
+      date : date,
+      megawatts : d3.projectionChart.randomIntGen(index * 3, (index * 3)+15)()
+    }
   })
 
   var pastData = data.filter(function(d) {
