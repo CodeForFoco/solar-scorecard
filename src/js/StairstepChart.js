371168adfb8d9bd03b009df6311fb11a1001a2c4
David Hayes <git@davidbhayes.com>
2018-08-02 19:52:02 -0600

    A more concise regex to parse dates from the API #BlameTim

--- src/js/StairstepChart.js
+++ src/js/StairstepChart.js
@@ -10,7 +10,7 @@ function prepareDataForDrawingOfStairstep(config) {
     const apiResponse = JSON.parse(config.data.fortCollins);
     let aggregatedByYear = {};
     apiResponse.forEach(r => {
-      let regex = /(\d+)-\d+-\d+T\d+:\d+:\d+/;
+      let regex = /(\d{4}).*/;
       const regSearchResult = regex.exec(r.date_of_service);
       if (regSearchResult == null) return;
       const year = regSearchResult[1];
