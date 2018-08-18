3d53a1a688630a272969240ebba79071ba16faef
David Hayes <git@davidbhayes.com>
2018-08-18 08:57:42 -0500

    Pushing the XHR request down so that it happens underneath the Vue initiation

--- src/js/main.js
+++ src/js/main.js
@@ -44,81 +44,82 @@ const router = new VueRouter({
 import Main from '../App.vue';
 
 // { selector : String, data :: Array [year, value] }
-export function drawSolarScorecardCharts(options) {
-  Tabs({
-    tabs: [
-      {
-        id: 'projections',
-        display: 'block',
-        label: 'Projections',
-        callback: function(element) {
-          element.innerHTML =
-            '<canvas id="stairstep-chart" width="600" height="400"></canvas>';
-          StairstepChart({
-            selector: '#stairstep-chart',
-            data: options.data,
-          });
+export function drawSolarScorecardCharts() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://opencity.fcgov.com/resource/ykei-s9zt.json');
+  // This is so Socrata and the City knows who we are and our app
+  //     it can often prevent API throttling
+  xhr.setRequestHeader('X-App-Token', 'uGISPdpNnBfwN4kmj9goaZBNM');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      let xhrDataContainer = {
+        data: {
+          fortCollins: []
         },
-      },
-      {
-        id: 'ratios',
-        display: 'none',
-        label: 'Ratios',
-        callback: function(element) {
-          element.innerHTML =
-            '<canvas id="pie-chart" width="600" height="400"></canvas>';
-
-          PieChart({
-            selector: '#pie-chart',
-            data: {
-              Electric: 50,
-              'Ground Travel': 26,
-              'Natural Gas': 19,
-              'Solid Waste': 4,
-              'Water Related': 0.3,
+        selector: '#stairstep-chart-tabs'
+      };
+      xhrDataContainer.data.fortCollins = xhr.responseText;
+      Tabs({
+        tabs: [
+          {
+            id: 'projections',
+            display: 'block',
+            label: 'Projections',
+            callback: function (element) {
+              element.innerHTML =
+                '<canvas id="stairstep-chart" width="600" height="400"></canvas>';
+              StairstepChart({
+                selector: '#stairstep-chart',
+                data: xhrDataContainer.data,
+              });
             },
-          });
-        },
-      },
-    ],
-  });
+          },
+          {
+            id: 'ratios',
+            display: 'none',
+            label: 'Ratios',
+            callback: function (element) {
+              element.innerHTML =
+                '<canvas id="pie-chart" width="600" height="400"></canvas>';
+
+              PieChart({
+                selector: '#pie-chart',
+                data: {
+                  Electric: 50,
+                  'Ground Travel': 26,
+                  'Natural Gas': 19,
+                  'Solid Waste': 4,
+                  'Water Related': 0.3,
+                },
+              });
+            },
+          },
+        ],
+      });
+    } else {
+      alert('FoCo API request failed.  Returned status of ' + xhr.status);
+    }
+  }
+  xhr.send();
 }
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://opencity.fcgov.com/resource/ykei-s9zt.json');
-// This is so Socrata and the City knows who we are and our app
-//     it can often prevent API throttling
-xhr.setRequestHeader('X-App-Token', 'uGISPdpNnBfwN4kmj9goaZBNM');
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    new Vue({
-      mounted() {
-        drawSolarScorecardCharts({
-          selector: '#stairstep-chart-tabs',
-          data: {
-            fortCollins: xhr.responseText,
-          },
-        });
-      },
-      router,
-      methods: {
-        goBack() {
-          window.history.length > 1 ?
-            this.$router.go(-1) :
-            this.$router.push('/');
-        },
-      },
-      computed: {
-        username() {
-          // We will see what `params` is shortly
-          return this.$route.params.username;
-        },
-      },
-      render: h => h(Main),
-    }).$mount('#app');
-  } else {
-    alert('FoCo API request failed.  Returned status of ' + xhr.status);
-  }
-};
-
-xhr.send();
+new Vue({
+  mounted() {
+    drawSolarScorecardCharts();
+  },
+  router,
+  methods: {
+    goBack() {
+      window.history.length > 1 ?
+        this.$router.go(-1) :
+        this.$router.push('/');
+    },
+  },
+  computed: {
+    username() {
+      // We will see what `params` is shortly
+      return this.$route.params.username;
+    },
+  },
+  render: h => h(Main),
+}).$mount('#app');
